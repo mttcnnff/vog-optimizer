8981dcf85fb2414df092d811ce37ed2aa959eeaf
Matthew Conniff <mvconniff@gmail.com>
2020-11-27 18:06:03 -0500

    make setup.py use reqs.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="vog_optimizer",
     version="0.0.1",
@@ -10,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mttcnnff/vog-optimizer",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
