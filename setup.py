15aa48d9dc9b581f016b4aa6fc5001376373f57e
Matthew Conniff <mvconniff@gmail.com>
2020-11-27 15:06:00 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="vog_optimizer",
+    version="0.0.1",
+    author="Matthew Conniff",
+    author_email="lol@nah.com",
+    description="A package for using VoG in an optimizer",
+    long_description="A long description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/mttcnnff/vog-optimizer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)
